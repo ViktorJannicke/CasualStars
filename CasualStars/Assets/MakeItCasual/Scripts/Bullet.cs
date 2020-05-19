4a1b7803b4e9e7faef005e48c09c75bb05257c8c
ViktorJannicke <57712656+ViktorJannicke@users.noreply.github.com>
2020-05-19 13:22:01 +0200

    Revert "Update Bullet.cs"
    This reverts commit d2ce5d3135bb6083aa5310a2cfb74216078f03dc.

--- CasualStars/Assets/MakeItCasual/Scripts/Bullet.cs
+++ CasualStars/Assets/MakeItCasual/Scripts/Bullet.cs
@@ -1,37 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using BeardedManStudios.Forge.Networking.Generated;
-using System.Threading;
 
-public class Bullet : BulletNetworkBehavior
+public class Bullet : MonoBehaviour
 {
-    public int bulletDamage = 0;
-    public float timer;
-    public float timeTillDeath;
-    private void Update()
+    // Start is called before the first frame update
+    void Start()
     {
-        if (networkObject == null)
-            return;
+        
+    }
 
-        if (!networkObject.IsOwner)
-        {
-            transform.position = networkObject.position;
-            transform.rotation = networkObject.rotation;
-        }
-        else
-        {
-            networkObject.position = transform.position;
-            networkObject.rotation = transform.rotation;
-        }
-
-        if (timer >= timeTillDeath)
-        {
-            networkObject.Destroy();
-        }
-        else if (timer < timeTillDeath)
-        {
-            timer += Time.deltaTime;
-        }
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
