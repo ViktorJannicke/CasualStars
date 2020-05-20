932494dd81e66a9e0bdd2cab20eb3f6b6cae9c7e
Talib47 <talib.akinci@gmail.com>
2020-05-20 16:02:56 +0200

    Meteriod Obstacle Script created

---
+++ CasualStars/Assets/MakeItCasual/Scenes/TalibScene/Obstacle.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Obstacle : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Movement movement;
+
+    private void Start()
+    {
+        movement.move(transform.position + new Vector3(1,0,0));
+    }
+
+    private void Update()
+    {
+        if (Vector3.Distance(agent.destination, transform.position) <= 1)
+        {
+            movement.move(NGameManager.manager.getNextPosition());
+        }
+    }
+}
