9fc7ed5c46e684cf856d9e9a4f1996e2b0c7dddb
teppich <johannesspicker@me.com>
2020-06-03 16:06:53 +0200

    scorboard mit jef
    jef ist back to the rules und regelt das scoreboard!

--- CasualStars/Assets/MakeItCasual/Scripts/Scoreboard.cs
+++ CasualStars/Assets/MakeItCasual/Scripts/Scoreboard.cs
@@ -1,18 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Scoreboard : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public static Scoreboard boardscore;
+	public GameObject scoreElement;
+	public List<PlayerData> ScoreData = new List<PlayerData>();
+	List<GameObject> Jef = new List<GameObject>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public void scoreboardzeichnen()
+	{
+
+		foreach (GameObject data in Jef)
+		{
+			Destroy(data);
+		}
+		Jef.Clear();
+
+			ScoreData.Sort((x, y) => x.score.CompareTo(y.score));
+
+		foreach(PlayerData data in ScoreData)
+		{
+			GameObject objectS = Instantiate(scoreElement, transform);
+			objectS.transform.GetChild(0).GetComponent<Text>().text = data.name;
+			objectS.transform.GetChild(1).GetComponent<Text>().text = data.score.ToString();
+			Jef.Add(objectS);
+		}	
+	}
+
+	private void Start()
+	{
+		if (boardscore != null)
+		{
+			Destroy(this);
+			return;
+		}
+
+		boardscore	 = this;
+	}
+
+
+
+
+
+
+
+
 }
