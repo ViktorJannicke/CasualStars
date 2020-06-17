ceeda5521efcdf21a9135ca48963c4653cfe2ac5
teppich <johannesspicker@me.com>
2020-06-17 15:53:08 +0200

    Werbung
    Hier könnte ihr werbung stehen sag ich da nur ansonsten ja werbung kommt am ende noch und da könnteihre werbung auch stehen sag ich nur ist nur ein tipp kostet auch nur 9.99€ pro werbe fläche

---
+++ CasualStars/Assets/MakeItCasual/Scenes/scriptwerbung.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scriptwerbung : MonoBehaviour
+{
+	float Zeit;
+	public float Max;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Zeit += Time.deltaTime;
+		if (Zeit >= Max)
+		{
+			SceneManager.LoadSceneAsync("Game");
+		}
+			
+    }
+}
