c5cb360ac3247d0945c23741a5efba175fe6dbef
teppich <johannesspicker@me.com>
2020-06-18 14:45:57 +0200

    explosionen
    knall bum ist am start jetzt gehts richtig los wer jagt hier alles hoch!?
    
    na ich natürlich!!XD

--- CasualStars/Assets/MakeItCasual/Scripts/Health.cs
+++ CasualStars/Assets/MakeItCasual/Scripts/Health.cs
@@ -10,23 +10,37 @@ public class Health : MonoBehaviour
 	public bool split;
 	public Obstacle obstacle;
 
+	public GameObject explosionSmall;
+	public GameObject explosionBig;
+	public float TTED = 10f;
+
 	// Start is called before the first frame update
 	private void OnCollisionEnter(Collision collision)
     {
 		if (collision.gameObject.CompareTag("Bullet") && lastHit != collision.gameObject)
 		{
-
 			lastHit = collision.gameObject;
-			health -= lastHit.GetComponent<Bullet>().bulletDamage;
-			Destroy(lastHit.GetComponent<Bullet>().gameObject);
+			health -= collision.gameObject.GetComponent<Bullet>().bulletDamage;
 			
 			if (health <= 0)
 			{
+				GameObject explosion = Instantiate(explosionBig, transform.position, transform.rotation);
+				explosion.transform.parent = null;
+				Destroy(explosion, TTED);
+
 				if (split || obstacle != null)
 				obstacle.Kill();
 				else
 				Destroy(gameObject);
 			}
+			else
+			{
+				GameObject explosion = Instantiate(explosionSmall, collision.gameObject.transform);
+				explosion.transform.parent = null;
+				Destroy(explosion, TTED);
+			}
+
+			Destroy(collision.gameObject);
 		}
     }
 }
