1355449e71df51e66866da197292dbb900635285
teppich <johannesspicker@me.com>
2020-07-29 11:14:48 +0200

    Laserbeamscript
    bald kann gelasert werden ihr dullies

---
+++ CasualStars/Assets/MakeItCasual/Scripts/LaserBeam.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserBeam : MonoBehaviour
+{
+	public GameObject laserPrefab;
+	public GameObject firePoint;
+
+	private GameObject spawnedLaser;
+
+	private void Start()
+	{
+		spawnedLaser = Instantiate(laserPrefab, firePoint.transform) as GameObject;
+
+		DisableLaser();
+	}
+
+	private void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			EnableLaser();
+		}
+
+		if (Input.GetMouseButton(0))
+		{
+			UpdateLaser();
+		}
+
+		if (Input.GetMouseButtonUp(0))
+		{
+			DisableLaser();
+		}
+	}
+
+	void EnableLaser()
+	{
+		spawnedLaser.SetActive(true);
+	}
+
+	void UpdateLaser()
+	{
+		if(firePoint != null)
+		{
+			spawnedLaser.transform.position = firePoint.transform.position;
+		}
+	}	
+
+	void DisableLaser()
+	{
+		spawnedLaser.SetActive(false);
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
